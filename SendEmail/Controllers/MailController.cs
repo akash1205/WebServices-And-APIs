4ccc76c159ea84a74761429bc4103e038b1a672b
Akash <akashjain1205@gmail.com>
2017-02-08 16:05:10 -0500

    Add files via upload

---
+++ SendEmail/Controllers/MailController.cs
@@ -0,0 +1,47 @@
+﻿using SendEmail.Models;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace SendEmail.Controllers
+{
+    public class MailController : ApiController
+    {
+        // GET: api/Mail
+        public string Get()
+        {
+            return "hey";
+        }
+
+        // GET: api/Mail/5
+        public HttpResponseMessage Get(int id)
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Forbidden);
+            return response;
+        }
+
+        // POST: api/Mail
+        public HttpResponseMessage Post([FromBody]Email value)
+        {
+            Models.Email email = new Models.Email();
+            
+            HttpStatusCode code= email.sendMail(value);
+            HttpResponseMessage response = Request.CreateResponse(code);
+            return response;
+        }
+
+        // PUT: api/Mail/5
+        public HttpResponseMessage Put(int id, [FromBody]string value)
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Forbidden);
+            return response;
+        }
+
+        // DELETE: api/Mail/5
+        public HttpResponseMessage Delete(int id)
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Forbidden);
+            return response;
+        }
+    }
+}
---
+++ SendEmail/Models/Email.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Web.Mail;
+namespace SendEmail.Models
+{
+    public class Email
+    {
+
+        private string server;
+        private int port;
+        private string username;
+        private string password;
+        public string customerEmail { get; set; }
+        public string serviceEmail { get; set; }
+        public string serviceName { get; set; }
+        public int bookingID { get; set; }
+        public DateTime bookingDate { get; set; }
+        public string slot { get; set; }
+        public Email()
+        {
+            server = "smtp.gmail.com";
+            port = 465;
+            username = "homeservices050@gmail.com";
+            password = "HomeService";
+        }
+
+        public HttpStatusCode sendMail(Email value)
+        {
+           try
+            {
+                MailMessage mailMsg = new MailMessage();
+
+                mailMsg.To = value.serviceEmail;
+                mailMsg.Headers.Add("From", string.Format("{0} <{1}>", "System", "homeservices050@gmail.com"));
+                mailMsg.Fields["http://schemas.microsoft.com/cdo/configuration/smtpserver"] = server;
+                mailMsg.Fields["http://schemas.microsoft.com/cdo/configuration/smtpserverport"] = port;
+                mailMsg.Fields["http://schemas.microsoft.com/cdo/configuration/sendusing"] = 2;
+                mailMsg.Fields["http://schemas.microsoft.com/cdo/configuration/smtpauthenticate"] = 1;
+                mailMsg.Fields["http://schemas.microsoft.com/cdo/configuration/sendusername"] = username;
+                mailMsg.Fields["http://schemas.microsoft.com/cdo/configuration/sendpassword"] = password;
+                mailMsg.Fields.Add("http://schemas.microsoft.com/cdo/configuration/smtpusessl", "true");
+               
+                mailMsg.BodyEncoding = Encoding.UTF8;
+                mailMsg.Subject = "Home Services -  You have been booked for "+value.serviceName;
+                mailMsg.Body = "Booking ID - " + value.bookingID + ". Appoitment on "+ value.bookingDate.ToLongDateString()+" at "+ value.slot;
+                    //+ bookingDate.Date+" at "+bookingDate.TimeOfDay;
+
+                SmtpMail.SmtpServer = server;
+                SmtpMail.Send(mailMsg);
+                mailMsg.To = value.customerEmail;
+                mailMsg.Subject = "Home Services -  Your service for " + value.serviceName+"has been booked";
+                mailMsg.Body = "Booking ID - " + value.bookingID + ". Appoitment on " + value.bookingDate.ToLongDateString() + " at " + value.slot;
+                
+                SmtpMail.Send(mailMsg);
+                return (HttpStatusCode.Accepted);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return (HttpStatusCode.BadRequest);
+            }
+
+        }
+
+
+    }
+}
